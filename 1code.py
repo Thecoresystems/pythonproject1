c9268b621722eb14db7f2a7f5d57e6b7782633f3
Thecoresystems <anchal.thecore@gmail.com>
2025-02-05 16:33:43 +0530

    Initial commit

---
+++ 1code.py
@@ -0,0 +1,10 @@
+import time 
+print (time.time())
+print ("my first python program")
+for i in range(4):
+    print("i am in side first for loop",i)
+    time.sleep(3)
+    
+for j in range(4):
+    print("     i am in side second for loop",j)    
+    time.sleep(1)
